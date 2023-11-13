6c510ba4a506fdde2acf25ea408a7e07fa0201f8
Arthur Duvillaret <aduvilla@bess-f4r3s14.clusters.42paris.fr>
2023-11-13 12:54:33 +0100

    GNL v2

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -5,6 +5,8 @@ int	get_next_line(const int fd, char **line)
 	char buff[BUFF_SIZE];
 
 	read(fd, buff, BUFF_SIZE);
+	while (ft_strchr(buff, '\n') == 0)
+
 	ft_strlcpy(*line, buff, BUFF_SIZE);
 	printf("valeur lu : %s", *line);
 	return (1);
