88b12cc0eb36114e78b6f32a95da730d131f7aa0
Arthur Duvillaret <aduvilla@bess-f4r3s14.clusters.42paris.fr>
2023-11-13 15:05:00 +0100

    GNL v3

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -2,13 +2,17 @@
 
 int	get_next_line(const int fd, char **line)
 {
-	char buff[BUFF_SIZE];
+	char	buff[BUFF_SIZE];
+	char	*tmp;
 
 	read(fd, buff, BUFF_SIZE);
-	while (ft_strchr(buff, '\n') == 0)
-
-	ft_strlcpy(*line, buff, BUFF_SIZE);
-	printf("valeur lu : %s", *line);
+	tmp =malloc(sizeof(char) * BUFF_SIZE);
+	tmp = ft_strchr(buff, '\n') + 2;
+	printf("reste du buffer : %s\n", tmp);
+	//printf("taille du buff moins avant ligne : %zu\n", ft_strlen(tmp));
+	ft_strlcat(*line, buff, BUFF_SIZE);
+	//ft_strlcat(*line, buff, (BUFF_SIZE - ft_strlen(ft_strchr(buff, '\n'))));
+	//printf("valeur lu : %s\n", *line);
 	return (1);
 	
 }
