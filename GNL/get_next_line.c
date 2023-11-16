e93736e23f09ba3afe5f07187fa3be591a25dd71
Arthur Duvillaret <aduvilla@bess-f4r3s14.clusters.42paris.fr>
2023-11-16 11:04:28 +0100

    GNL v11

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -1,6 +1,6 @@
 #include "get_next_line.h"
 
-static int	isend(char *s)
+int	isend(char *s)
 {
 	int	i;
 
@@ -8,70 +8,53 @@ static int	isend(char *s)
 	while (s[i])
 	{
 		if (s[i] == '\n')
-			return (i);
+			return (1);
 	i++;
 	}
 	return (0);
 }
-/*
-static int	count_index(char *buf, size_t bsize)
-{
-	size_t	i;
-	int		count;
 
-	i = 0;
-	count = 0;
-	while(buf[i] && i < bsize)
+char	*read_to_tmp(char *tmp, const int fd)
+{
+	char	buff[BUFF_SIZE + 1];
+	char	*cpy;
+	int	i;
+
+	i = 1;
+	while (i > 0)
 	{
-		if (buf[i] == '\n')
-			return (count);
-		i++;
-		count++;
+		i = read(fd, buff, BUFF_SIZE);
+		if (i < 0)
+		{
+			free(tmp);
+			return (NULL);
+		}
+		else if (i == 0)
+			return (tmp);
+		buff[i] = '\0';
+		if (!tmp)
+			tmp = buff;
+		else
+		{
+			cpy = tmp;
+			tmp = ft_strjoin(cpy, buff);
+			free(cpy);
+			cpy = NULL;
+		}
+		if (isend(buff))
+			return (tmp);
 	}
-	return (count);
-}
-static char	*split_buff(char *s)
-{
-
-}
-*/
-
-static void	read_to_tmp(char *tmp, const int fd)
-{
-	char	buff[BUFF_SIZE];
-	static int	i;
-
-	i = i + read(fd, buff, BUFF_SIZE);
-	printf("buff : %s\n", buff);
-	printf("lu : %d\n", i);
-	printf("tmp : %s\n", tmp);
-	ft_strlcat(tmp, buff, i + 1);
+	return (tmp);
 }
 
 char	*get_next_line(const int fd)
 {
-	static char	tmp[255];
-//	char		*result;
+	static char	*tmp;
+	char		*result;
 
-	if (fd < 0 || BUFF_SIZE <= 0 || read(fd, tmp, 0) < 0)
+	if (fd < 0 || BUFF_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	while (!isend(tmp))
-	{
-		read_to_tmp(tmp, fd);
-		printf("tmp : %s\n", tmp);
-	}
-//	result = split_buff(tmp);
-//	printf("result : %s\n", result);
-//	return (result);
-	/*i = count_index(buff, BUFF_SIZE);
-	index = index + i;
-	ft_strlcat(*line, buff, index + 1);
-	tmp = ft_substr(buff, i, BUFF_SIZE - i);
-	printf("Taille a copier : %zu\n", i); 
-	printf("GNL : %s\n", *line);
-	printf("reste du buffer : %s\n", tmp);
-	read(fd, buff, BUFF_SIZE);
-	*/
-	return (tmp);
-
+	result = read_to_tmp(tmp, fd);
+	printf("result : %s\n", result);
+	return (result);
 }
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,18 +6,28 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 09:29:08 by aduvilla          #+#    #+#             */
-/*   Updated: 2023/11/15 14:14:32 by aduvilla         ###   ########.fr       */
+/*   Updated: 2023/11/16 10:59:57 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFF_SIZE 42
 
-# include "libft.h"
+# include <unistd.h>
+# include <stdlib.h>
 # include <stdio.h>
+# include <fcntl.h>
 
-#define BUFF_SIZE 4
+
 
 char	*get_next_line(const int fd);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*read_to_tmp(char *tmp, const int fd);
+int		isend(char *s);
+
+
 
 #endif
