925328c82dbce4464b28a40357f772b46a5f0b87
C0rvax <arthurduvillaret@gmail.com>
2023-12-15 16:10:50 +0100

    debut de so_long v2

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -1,3 +1,5 @@
+#include "libft.h"
+#include "ft_printf.h"
 #include <mlx.h>
 
 int	main(void)
@@ -6,6 +8,8 @@ int	main(void)
 	void	*mlx_win;
 
 	mlx = mlx_init();
+	ft_putstr("maybe\n");
+//	ft_printf("Youhou ca marche");
 	mlx_win = mlx_new_window(mlx, 800, 600, "Hello world!");
 	mlx_loop(mlx);
 }
