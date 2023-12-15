10c7fb3b6b91c0ad101306a4bb417e6382a729b4
C0rvax <arthurduvillaret@gmail.com>
2023-12-15 17:05:56 +0100

    debut de so_long v4

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -5,13 +5,13 @@
 int	main(void)
 {
 	void	*mlx;
-	void	*mlx_win;
+//	void	*mlx_win;
 
 	mlx = mlx_init();
 	ft_putstr("maybe\n");
 	print_putstr("allez\n");
 //	ft_printf("Youhou ca marche");
-	mlx_win = mlx_new_window(mlx, 800, 600, "Hello world!");
+	mlx_new_window(mlx, 800, 600, "Hello world!");
 	mlx_loop(mlx);
 }
 
