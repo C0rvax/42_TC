5bcb2498f043c996452d923fb00ab76da1f9b5d4
Arthur Duvillaret <aduvilla@bess-f4r3s14.clusters.42paris.fr>
2024-01-23 16:43:33 +0100

    avec testeur shell v3

--- pipex/exec.sh
+++ pipex/exec.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
 
-make
+# Couleurs
+rouge='\e[1;31m'
+jaune='\e[1;33m'
+bleu='\e[1;34m'
+violet='\e[1;35m'
+vert='\e[1;32m'
+neutre='\e[0;m'
 
-valgrind --leak-check=full --trace-children=yes ./pipex infile "${CMD1[i]}" "${CMD2[i]}" outfile2
+function check_outfile {
+	diff --brief outfile outfile2 >/dev/null
+	comp_value=$?
 
-valgrind --leak-check=full --trace-children=yes ./pipex infile "${CMD1[i]}" "${CMD2[i]}" outfile2
+	if [ $comp_value -eq 1 ]; then
+		echo -e "${rouge}[KO]${neutre}"
+	else
+		echo -e "${vert}[OK]${neutre}"
+	fi
+	rm outfile outfile2
+}
 
-diff --brief <(sort file1) <(sort file2) >/dev/null
-comp_value=$?
+make >/dev/null
 
-if [ $comp_value -eq 1 ]; then
-	echo "do something because they're different"
-else
-	echo "do something because they're identical"
-fi
+echo -e "${bleu} TEST : grep la | wc -w ${neutre}"
+valgrind --leak-check=full --trace-children=yes ./pipex infile "grep la" "wc -w" outfile
+valgrind --leak-check=full --trace-children=yes grep 'la' <infile | wc -w >outfile2
+check_outfile
 
-make fclean
+echo -e "${bleu} TEST : grep la | grep - ${neutre}"
+valgrind --leak-check=full --trace-children=yes ./pipex infile "grep la" "grep -" outfile
+valgrind --leak-check=full --trace-children=yes grep 'la' <infile | grep '-' >outfile2
+check_outfile
+
+echo -e "${bleu} TEST : grep la | wc -w | wc -w ${neutre}"
+valgrind --leak-check=full --trace-children=yes ./pipex infile "grep la" "grep -" "wc -w" outfile
+valgrind --leak-check=full --trace-children=yes grep 'la' <infile | grep '-' | wc -w >outfile2
+check_outfile
+
+echo -e "${bleu} TEST : grep la | cat | wc -w ${neutre}"
+valgrind --leak-check=full --trace-children=yes ./pipex infile "grep la" "cat" "wc -w" outfile
+valgrind --leak-check=full --trace-children=yes grep 'la' <infile | cat | wc -w >outfile2
+check_outfile
+
+echo -e "${bleu} TEST : grep la | cat | wc -w ${neutre}"
+./pipex infile "grep la" "cat" "wc -w" outfile
+grep 'la' <infile | cat | wc -w >outfile2
+check_outfile
+make fclean >/dev/null
