24d7b13035802693beeac1ad83d437e55bd0d02f
Arthur Duvillaret <aduvilla@bess-f4r3s14.clusters.42paris.fr>
2024-03-04 11:28:15 +0100

    nickel

--- push_swap/bonus/check_list.c
+++ push_swap/bonus/check_list.c
@@ -6,7 +6,7 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 18:27:58 by aduvilla          #+#    #+#             */
-/*   Updated: 2024/03/03 01:11:24 by aduvilla         ###   ########.fr       */
+/*   Updated: 2024/03/04 11:27:55 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,56 +58,6 @@ static int	check_dup(int *a, int size)
 	return (1);
 }
 
-static int	check_max(char **tab)
-{
-	int		i;
-	char	*max;
-	char	*cpy;
-
-	i = 0;
-	max = "2147483647";
-	while (tab[i])
-	{
-		if (tab[i][0] != '-')
-		{
-			cpy = tab[i];
-			if (*cpy == '+')
-				cpy++;
-			while (*cpy == '0')
-				cpy++;
-			if (ft_strlen(cpy) > 10 || (ft_strlen(cpy) == 10
-					&& ft_strncmp(cpy, max, 10) > 0))
-				return (ft_putstr_fd("Error\n", 2), 0);
-		}
-		i++;
-	}
-	return (1);
-}
-
-static int	check_min(char **tab)
-{
-	int		i;
-	char	*min;
-	char	*cpy;
-
-	i = 0;
-	min = "2147483648";
-	while (tab[i])
-	{
-		if (tab[i][0] == '-')
-		{
-			cpy = tab[i] + 1;
-			while (*cpy == '0')
-				cpy++;
-			if (ft_strlen(cpy) > 10 || (ft_strlen(cpy) == 10
-					&& ft_strncmp(cpy, min, 10) > 0))
-				return (ft_putstr_fd("Error\n", 2), 0);
-		}
-		i++;
-	}
-	return (1);
-}
-
 static int	check_min_max(char **tab)
 {
 	int		i;
@@ -140,7 +90,7 @@ int	*check_list(char **args, int size)
 
 	if (!check_int(args, size))
 		return (ft_putstr_fd("Error\n", 2), NULL);
-	if (!check_min(args) || !check_max(args))
+	if (!check_min_max(args))
 		return (NULL);
 	a = malloc(sizeof(int) * size);
 	if (!a)
@@ -155,57 +105,3 @@ int	*check_list(char **args, int size)
 		return (ft_putstr_fd("Error\n", 2), free(a), NULL);
 	return (a);
 }
-/*
-static int	check_max(char **tab)
-{
-	int		i;
-	char	*max;
-	size_t	len;
-
-	i = 0;
-	len = 10;
-	max = "+2147483647";
-	while (tab[i])
-	{
-		if (tab[i][0] != '-')
-		{
-			if (tab[i][0] == '+')
-				len++;
-			if (ft_strlen(tab[i]) > len)
-				return (ft_putstr_fd("Error\n", 2), 0);
-			else if (ft_strlen(tab[i]) == len)
-			{
-				if ((len == 10 && ft_strncmp(tab[i], "2147483647", 10) > 0)
-					|| (len == 11 && ft_strncmp(tab[i], max, 11) > 0))
-					return (ft_putstr_fd("Error\n", 2), 0);
-			}
-		}
-		i++;
-	}
-	return (1);
-}
-
-static int	check_min(char **tab)
-{
-	int		i;
-	char	*min;
-
-	i = 0;
-	min = "-2147483648";
-	while (tab[i])
-	{
-		if (tab[i][0] == '-')
-		{
-			if (ft_strlen(tab[i]) > 11)
-				return (ft_putstr_fd("Error\n", 2), 0);
-			else if (ft_strlen(tab[i]) == 11)
-			{
-				if (ft_strncmp(tab[i], min, 11) > 0)
-					return (ft_putstr_fd("Error\n", 2), 0);
-			}
-		}
-		i++;
-	}
-	return (1);
-}
-*/
