9eed79d01c6d6237e8ce940ba17ade67cff89629
c0rvax <C0rvax@pm.me>
2024-09-23 23:37:02 +0200

    on recommence

---
+++ CPP/M1/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 23:15:43 by aduvilla          #+#    #+#             */
+/*   Updated: 2024/09/23 23:34:46 by aduvilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (i = 1; i < ac; i++)
+		{
+			for (j = 0; av[i][j]; j++)
+				std::cout << (char) std::toupper(av[i][j]) << std::endl;
+		}
+	}
+	return (0);
+
+
+}
