09c7a83e016cd7b21122327721f46672f06eb7e3
c0rvax <C0rvax@pm.me>
2024-10-03 20:21:38 +0200

    ex06

---
+++ CPP/CPP_01/ex06/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 19:39:38 by aduvilla          #+#    #+#             */
+/*   Updated: 2024/10/03 20:21:18 by aduvilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+#include <iostream>
+
+int main()
+{
+    Harl	harl;
+
+    harl.complain("DEBUG");
+	std::cout << std::endl;
+    harl.complain("INFO");
+	std::cout << std::endl;
+    harl.complain("WARNING");
+	std::cout << std::endl;
+    harl.complain("ERROR");
+	std::cout << std::endl;
+    harl.complain("NOSE");
+	std::cout << std::endl;
+    harl.complain("DEBUGI");
+	std::cout << std::endl;
+    harl.complain("INFOA");
+	std::cout << std::endl;
+    harl.complain("WARNINGU");
+	std::cout << std::endl;
+    harl.complain("ERRORO");
+
+    return 0;
+}
