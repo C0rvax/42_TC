3ce7f0fb02ca3a1170c3c9dd08acbfb94f259042
c0rvax <C0rvax@pm.me>
2024-10-25 18:52:02 +0200

    microshell corrigé

---
+++ microshell.c
@@ -0,0 +1,96 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int	print_error(char *str, char *arg, int status)
+{
+	while (*str)
+		write(2, str++, 1);
+	while (arg && *arg)
+		write(2, arg++, 1);
+	write(2, "\n", 1);
+	return (status);
+}
+
+void	set_pipe(int *fd, int fileno)
+{
+	if (dup2(fd[fileno], fileno) == -1)
+		exit(print_error("error: fatal", NULL, 1));
+	close(fd[0]);
+	close(fd[1]);
+}
+
+int	exec_cd(char **av, int i)
+{
+	if (i != 2)
+		return (print_error("error: cd: bad arguments", NULL, 1));
+	if (chdir(av[1]) != 0)
+		return (print_error("error: cd: cannot change directory to ", av[1], 1));
+	return (0);
+}
+
+int	ft_exec(char **av, int i, char **env)
+{
+	int	fd[2];
+	int	status;
+	int	pid;
+
+	status = 0;
+	if (!strcmp(av[0], "cd"))
+		return (exec_cd(av, i));
+	if (!av[i] || !strcmp(av[i], ";"))
+	{
+		pid = fork();
+		if (pid == -1)
+			exit(print_error("error: fatal", NULL, 1));
+		if (pid == 0)
+		{
+			av[i] = NULL;
+			execve(av[0], av, env);
+			exit(print_error("error: cannot execute ", av[0], 1));
+		}
+		while(waitpid(-1, &status, 0))
+			;
+	}
+	else if (!strcmp(av[i], "|"))
+	{
+		if (pipe(fd) == -1)
+			exit(print_error("error: fatal", NULL, 1));
+		pid = fork();
+		if (pid == -1)
+			exit(print_error("error: fatal", NULL, 1));
+		if (pid == 0)
+		{
+			av[i] = NULL;
+			set_pipe(fd, 1);
+			execve(av[0], av, env);
+			exit(print_error("error: cannot execute ", av[0], 1));
+		}
+		while(waitpid(-1, &status, 0))
+			;
+		set_pipe(fd, 0);
+	}
+	return (WIFEXITED(status) && WEXITSTATUS(status));
+}
+
+int	main(int ac, char **av, char **env)
+{
+	int	status;
+	int	i;
+
+	status = 0;
+	i = 0;
+	if (ac < 2)
+		return (0);
+	while (av[i])
+	{
+		av += i + 1;
+		i = 0;
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
+			i++;
+		if (i)
+			status = ft_exec(av, i, env);
+	}
+	return (status);
+}
