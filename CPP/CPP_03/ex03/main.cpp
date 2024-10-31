ed706efc4baa4ae23c849600e2d94aa7adeaede5
c0rvax <C0rvax@pm.me>
2024-10-31 13:20:21 +0100

    all good

--- CPP/CPP_03/ex03/main.cpp
+++ CPP/CPP_03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 21:47:45 by aduvilla          #+#    #+#             */
-/*   Updated: 2024/10/31 12:23:19 by aduvilla         ###   ########.fr       */
+/*   Updated: 2024/10/31 13:19:18 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,22 @@ int main (void)
 {
 	DiamondTrap a("Yves");
 	std::cout << std::endl;
-	a.printTrap();
-	std::cout << std::endl;
 	DiamondTrap	b( a );
 
 	std::cout << std::endl;
 	b.printTrap();
+	std::cout << std::endl;
 	a.printTrap();
+	std::cout << std::endl;
 	a.attack("robert");
 	a.beRepaired(20);
 	a.takeDamage(30);
+	std::cout << std::endl;
 	a.guardGate();
+	std::cout << std::endl;
 	a.highFivesGuys();
+	std::cout << std::endl;
 	a.whoAmI();
+	std::cout << std::endl;
 	return 0;
 }
