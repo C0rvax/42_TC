6037fff5a8c5691b0cf6398dabc475592d0e213a
c0rvax <C0rvax@pm.me>
2024-11-12 16:58:08 +0100

    debut du 5-0

---
+++ CPP/CPP_05/ex00/include/Bureaucrat.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 15:52:35 by aduvilla          #+#    #+#             */
+/*   Updated: 2024/11/12 16:56:55 by aduvilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __BUREAUCRAT_HPP__
+#define __BUREAUCRAT_HPP__
+
+#include <string>
+
+class	Bureaucrat
+{
+	public:
+		Bureaucrat	(void);
+		Bureaucrat	(std::string name, int grade);
+		Bureaucrat	(Bureaucrat const & src);
+		Bureaucrat&	operator=(Bureaucrat const & rhs);
+		~Bureaucrat	(void);
+
+	std::string	getName(void) const;
+	int			getGrade(void) const;
+	Bureaucrat&	operator++(void);
+	Bureaucrat	operator++(int);
+	Bureaucrat&	operator--(void);
+	Bureaucrat	operator--(int);
+
+	class	GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char *	what(void) const throw();
+	};
+
+	class	GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char *	what(void) const throw();
+	};
+
+	private:
+	std::string const	m_name;
+	int					m_grade;
+	static const int	m_maxGrade = 1;
+	static const int	m_minGrade = 150;
+};
+
+std::ostream&	operator<<(std::ostream & o, Bureaucrat const & rhs);
+#endif  /* __BUREAUCRAT_HPP__ */
