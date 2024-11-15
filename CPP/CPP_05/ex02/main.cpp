161eb7ffdc05c90f9ee1b42e676a3ffca78ed489
c0rvax <C0rvax@pm.me>
2024-11-15 12:07:16 +0100

    changement main 5-2

--- CPP/CPP_05/ex02/main.cpp
+++ CPP/CPP_05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 16:52:15 by aduvilla          #+#    #+#             */
-/*   Updated: 2024/11/14 18:14:52 by aduvilla         ###   ########.fr       */
+/*   Updated: 2024/11/15 12:05:10 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,11 +39,11 @@ int	main()
 	promoted.executeForm(robo);
 	promoted.executeForm(pres);
 	std::cout << "promotion of " << promoted++ <<std::endl;
+	promoted.executeForm(robo);
 	std::cout << std::endl;
 	std::cout << shrub << std::endl;
 	std::cout << robo << std::endl;
 	std::cout << pres << std::endl;
-	promoted.executeForm(robo);
 	boss.executeForm(pres);
 	boss.signForm(pres);
 	boss.executeForm(pres);
