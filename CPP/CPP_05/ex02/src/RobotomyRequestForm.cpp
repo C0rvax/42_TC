8ae5750cffa03c127945f2db1ac17311e380a3b2
c0rvax <C0rvax@pm.me>
2025-02-05 14:03:12 +0100

    review etienne

--- CPP/CPP_05/ex02/src/RobotomyRequestForm.cpp
+++ CPP/CPP_05/ex02/src/RobotomyRequestForm.cpp
@@ -6,7 +6,7 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:18:18 by aduvilla          #+#    #+#             */
-/*   Updated: 2024/11/15 14:33:47 by aduvilla         ###   ########.fr       */
+/*   Updated: 2025/02/05 14:00:17 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,7 @@ RobotomyRequestForm::~RobotomyRequestForm	(void)
 
 void	RobotomyRequestForm::execute(Bureaucrat const & executor) const
 {
+	srand(time(0));
 	if (!this->getSigned())
 		throw AForm::UnsignedException();
 	if (this->getExecuteGrade() < executor.getGrade())
--- CPP/CPP_05/ex02/src/main.cpp
+++ CPP/CPP_05/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 16:52:15 by aduvilla          #+#    #+#             */
-/*   Updated: 2024/11/15 12:05:10 by aduvilla         ###   ########.fr       */
+/*   Updated: 2025/02/05 14:01:15 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,33 @@ int	main()
 	RobotomyRequestForm		robo("bob");
 	PresidentialPardonForm	pres("bob");
 
-	slave.signForm(shrub);	
-	slave.signForm(robo);	
-	slave.signForm(pres);	
-	promoted.executeForm(shrub);
-	promoted.executeForm(robo);
-	promoted.executeForm(pres);
-	promoted.signForm(shrub);
-	promoted.signForm(robo);
-	promoted.signForm(pres);
-	promoted.executeForm(shrub);
-	promoted.executeForm(robo);
-	promoted.executeForm(pres);
-	std::cout << "promotion of " << promoted++ <<std::endl;
-	promoted.executeForm(robo);
-	std::cout << std::endl;
-	std::cout << shrub << std::endl;
-	std::cout << robo << std::endl;
-	std::cout << pres << std::endl;
-	boss.executeForm(pres);
-	boss.signForm(pres);
-	boss.executeForm(pres);
+	try
+	{
+		slave.signForm(shrub);	
+		slave.signForm(robo);	
+		slave.signForm(pres);	
+		promoted.executeForm(shrub);
+		promoted.executeForm(robo);
+		promoted.executeForm(pres);
+		promoted.signForm(shrub);
+		promoted.signForm(robo);
+		promoted.signForm(pres);
+		promoted.executeForm(shrub);
+		promoted.executeForm(robo);
+		promoted.executeForm(pres);
+		std::cout << "promotion of " << promoted++ <<std::endl;
+		promoted.executeForm(robo);
+		std::cout << std::endl;
+		std::cout << shrub << std::endl;
+		std::cout << robo << std::endl;
+		std::cout << pres << std::endl;
+		boss.executeForm(pres);
+		boss.signForm(pres);
+		boss.executeForm(pres);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	return 0;
 }
