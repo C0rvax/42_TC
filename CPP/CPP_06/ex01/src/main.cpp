c38b40c2bedaa8bb511911596e781638d6e5fe17
c0rvax <C0rvax@pm.me>
2025-02-05 20:30:42 +0100

    ok 6-1

--- CPP/CPP_06/ex01/src/main.cpp
+++ CPP/CPP_06/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 15:14:34 by aduvilla          #+#    #+#             */
-/*   Updated: 2025/01/22 16:01:38 by aduvilla         ###   ########.fr       */
+/*   Updated: 2025/02/05 20:21:15 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ int	main()
 
 	std::cout << (deserializedRobert == &robert ? "struct are the same" : "struct are differents") << std::endl;
 	std::cout << "OG name: " << robert.name << " / level: " << robert.level << std::endl;
-	std::cout << "New name: " << deserializedRobert->name << " / level: " << deserializedRobert->level << std::endl;
+	std::cout << "Deserialized name: " << deserializedRobert->name << " / level: " << deserializedRobert->level << std::endl;
 	return 0;
 }
