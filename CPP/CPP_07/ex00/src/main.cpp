95019c8cf47bbb5713b3a544db90b8a658a6e7cc
aduvilla <aduvilla@student.42.fr>
2025-02-17 10:36:31 +0100

    ajout test ex00

--- CPP/CPP_07/ex00/src/main.cpp
+++ CPP/CPP_07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:48:32 by aduvilla          #+#    #+#             */
-/*   Updated: 2025/01/22 16:58:57 by aduvilla         ###   ########.fr       */
+/*   Updated: 2025/02/17 10:30:17 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,24 @@ int	main()
 	int	b = 3;
 
 	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "a = " << a << ", b = " << b << "\n";
+	std::cout << "min( a, b ) = " << ::min( a, b ) << "\n";
+	std::cout << "max( a, b ) = " << ::max( a, b ) << "\n";
 	
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 
 	::swap( c, d );
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "\nc = " << c << ", d = " << d << "\n";
+	std::cout << "min( c, d ) = " << ::min( c, d ) << "\n";
+	std::cout << "max( c, d ) = " << ::max( c, d ) << "\n";
 
+	float	e = 42.2;
+	float	f = 41.8;
+
+	::swap(e, f);
+	std::cout << "\ne = " << e << ", f = " << f << "\n";
+	std::cout << "min( e, f ) = " << ::min( e, f ) << "\n";
+	std::cout << "max( e, f ) = " << ::max( e, f ) << "\n";
 	return 0;
 }
--- CPP/CPP_07/ex01/src/main.cpp
+++ CPP/CPP_07/ex01/src/main.cpp
@@ -6,12 +6,13 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 17:29:53 by aduvilla          #+#    #+#             */
-/*   Updated: 2025/01/23 19:01:49 by aduvilla         ###   ########.fr       */
+/*   Updated: 2025/02/17 10:35:36 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Iter.hpp"
 #include <cctype>
+#include <iostream>
 #include <string>
 
 void	printSquare(int & value)
@@ -35,19 +36,20 @@ void	printLoud(std::string & str)
 int	main()
 {
 	int	intA[] = { 2, 4, 8, 16, 32, 64};
+	std::cout << "Int Array:\n";
 	iter<int>(intA, static_cast<unsigned int>(sizeof(intA) / sizeof(intA[0])), printIt<int>);
-	std::cout << std::endl;
+	std::cout << "\nInt Array squared:\n";
 	iter<int>(intA, static_cast<unsigned int>(sizeof(intA) / sizeof(intA[0])), printSquare);
-	std::cout << std::endl;
 
+	std::cout << "\nChar Array:\n";
 	char	charA[] = { 't', 'o', ' ', 'u', 'p', 'p', 'e', 'r'};
 	iter<char>(charA, static_cast<unsigned int>(sizeof(charA) / sizeof(charA[0])), printIt<char>);
-	std::cout << std::endl;
+	std::cout << "\nChar Array capitalized:\n";
 	iter<char>(charA, static_cast<unsigned int>(sizeof(charA) / sizeof(charA[0])), printMaj);
-	std::cout << std::endl;
 
+	std::cout << "\nString Array:\n";
 	std::string	strA[] = { "hello", "world", "!"};
 	iter<std::string>(strA, static_cast<unsigned int>(sizeof(strA) / sizeof(strA[0])), printIt<std::string>);
-	std::cout << std::endl;
+	std::cout << "\nString Array loud:\n";
 	iter<std::string>(strA, static_cast<unsigned int>(sizeof(strA) / sizeof(strA[0])), printLoud);
 }
