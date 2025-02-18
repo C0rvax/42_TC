b327ddbdd97c00d2e4d518f0948a9af90a23eda4
aduvilla <aduvilla@student.42.fr>
2025-02-18 14:27:20 +0100

    main ex00 plus propre

--- CPP/CPP_08/ex01/src/main.cpp
+++ CPP/CPP_08/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 12:01:23 by aduvilla          #+#    #+#             */
-/*   Updated: 2025/02/05 16:30:06 by aduvilla         ###   ########.fr       */
+/*   Updated: 2025/02/18 14:26:26 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main()
 {
 	try
 	{
-		std::cout << "Add to empty:" << std::endl;
+		std::cout << "Add to empty:" << "\n";
 		Span	emptyS(0);
 		emptyS.addNumber(2);
 	}
@@ -27,8 +27,7 @@ int	main()
 	}
 	try
 	{
-		std::cout << std::endl;
-		std::cout << "try to span between 1 number" << std::endl;
+		std::cout << "\ntry to span between 1 number\n";
 		Span	small(100);
 		small.addNumber(2);
 		small.shortestSpan();
@@ -40,8 +39,7 @@ int	main()
 	}
 	try
 	{
-		std::cout << std::endl;
-		std::cout << "try to add too many numbers" << std::endl;
+		std::cout << "\ntry to add too many numbers\n";
 		Span	large(10000);
 		large.addManyNumbers(99999999, 88);
 	}
@@ -51,17 +49,16 @@ int	main()
 	}
 	try
 	{
-		std::cout << std::endl;
 		Span	veryLarge(10000000);
 		veryLarge.addManyNumbers(99, 88);
-		std::cout << "Add 99 numbers with offset 1 starting at 88:" << std::endl;
-		std::cout << "shorterstSpan: " << veryLarge.shortestSpan() << std::endl;
-		std::cout << "longestSpan: " << veryLarge.longestSpan() << std::endl;
+		std::cout << "\nAdd 99 numbers with offset 1 starting at 88:\n";
+		std::cout << "shorterstSpan: " << veryLarge.shortestSpan() << "\n";
+		std::cout << "longestSpan: " << veryLarge.longestSpan() << "\n";
 		veryLarge.addManyNumbers(500, -500, 40);
-		std::cout << "Add 500 numbers with offset 40 starting at -500:" << std::endl;
-		std::cout << "shorterstSpan: " << veryLarge.shortestSpan() << std::endl;
-		std::cout << "longestSpan: " << veryLarge.longestSpan() << std::endl;
-		std::cout << "499 x 40 = " << 499 * 40 << std::endl;
+		std::cout << "Add 500 numbers with offset 40 starting at -500:\n";
+		std::cout << "shorterstSpan: " << veryLarge.shortestSpan() << "\n";
+		std::cout << "longestSpan: " << veryLarge.longestSpan() << "\n";
+		std::cout << "499 x 40 = " << 499 * 40 << "\n";
 		veryLarge.addManyNumbers(0, -500, 40);
 	}
 	catch (const std::exception& e)
@@ -70,16 +67,15 @@ int	main()
 	}
 	try
 	{
-		std::cout << std::endl;
 		Span	negative(10000);
 		negative.addManyNumbers(500, -500000, 40);
-		std::cout << "Add 500 numbers with offset 40 starting at -500000:" << std::endl;
-		std::cout << "shorterstSpan: " << negative.shortestSpan() << std::endl;
-		std::cout << "longestSpan: " << negative.longestSpan() << std::endl;
+		std::cout << "\nAdd 500 numbers with offset 40 starting at -500000:\n";
+		std::cout << "shorterstSpan: " << negative.shortestSpan() << "\n";
+		std::cout << "longestSpan: " << negative.longestSpan() << "\n";
 	}
 	catch (const std::exception& e)
 	{
 		std::cout << e.what() << std::endl;
 	}
-
+	return 0;
 }
