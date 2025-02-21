a6e61cee2e5c22509729bcb24e30314a6311e1bc
aduvilla <aduvilla@sudent.42.fr>
2025-02-21 12:41:37 +0100

    essais

--- CPP/CPP_00/ex00/megaphone.cpp
+++ CPP/CPP_00/ex00/megaphone.cpp
@@ -6,28 +6,28 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 23:15:43 by aduvilla          #+#    #+#             */
-/*   Updated: 2024/09/30 13:31:59 by aduvilla         ###   ########.fr       */
+/*   Updated: 2025/02/21 11:41:09 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
 	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	else
 	{
 		for (i = 1; i < ac; i++)
 		{
 			for (j = 0; av[i][j]; j++)
-				std::cout << (char) std::toupper(av[i][j]);
+				std::cout << (char)std::toupper(av[i][j]);
 			std::cout << ' ';
 		}
-		std::cout << std::endl;
+		std::cout << "\n";
 	}
-	return (0);
+	return 0;
 }
