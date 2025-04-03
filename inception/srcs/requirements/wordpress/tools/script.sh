20d74c145ac17f72f863e23316ab31a31a6abfe2
aduvilla <aduvilla@sudent.42.fr>
2025-04-03 18:20:27 +0200

    mondatory ok

--- inception/srcs/requirements/wordpress/tools/script.sh
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -9,10 +9,10 @@ mv wp-cli.phar /usr/local/bin/wp # Use wp instead of wp-cli...
 
 wp core download --allow-root
 wp config create --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$USER_PWD --dbhost=mariadb --allow-root
-wp core install --url=$DOMAIN_NAME/ --title=$TITLE --admin_user=$ADM --admin_password=$ADM_PWD --admin_email=$EMAIL --skip-email --allow-root
-wp user create user1 user1@example.com --role=author --user_pass=$USER_PWD --allow-root
+wp core install --url=$DOMAIN_NAME/ --title=$TITLE --admin_user=$WP_ADM --admin_password=$ADM_PWD --admin_email=$WP_ADM_EMAIL --skip-email --allow-root
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
-unset USER_PWD ADM_PWD ROOT_PWD
+unset USER_PWD ADM_PWD ROOT_PWD WP_PWD
 
 cd
 
