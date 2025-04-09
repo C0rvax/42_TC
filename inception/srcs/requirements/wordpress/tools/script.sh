e36ea313e19b118ccbdea5df2da1710a2222cad4
aduvilla <aduvilla@sudent.42.fr>
2025-04-09 22:52:28 +0200

    fin secrets et site en docker

--- inception/srcs/requirements/wordpress/tools/script.sh
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -7,13 +7,12 @@ REDIS_PWD=$(cat "$REDIS_PWD_FILE")
 
 if [ ! -f /var/www/html/wp-config.php ]; then
 	cd /var/www/html
-	#rm -rf * # clean
+
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
 
 	wp core download --allow-root
-
 	wp config create --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$USER_PWD --dbhost=mariadb --allow-root
 	wp core install --url="${DOMAIN_NAME%/}" --title=$TITLE --admin_user=$WP_ADM --admin_password=$ADM_PWD --admin_email=$WP_ADM_EMAIL --skip-email --allow-root
 	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_PWD --allow-root
@@ -21,11 +20,14 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	# REDIS CONFIG
 	wp config set FS_METHOD direct --allow-root --type=constant
 	wp config set WP_REDIS_HOST redis --allow-root --type=constant
-	wp config set WP_REDIS_PORT 6379 --allow-root --type=constant --raw # Port is numeric
+	wp config set WP_REDIS_PORT 6379 --allow-root --type=constant --raw
 	wp config set WP_REDIS_PASSWORD "$REDIS_PWD" --allow-root --type=constant
 
 	wp plugin install redis-cache --activate --allow-root
 	wp redis enable --allow-root
+	if [ -f /subject.html ]; then
+		mv /subject.html /var/www/html/subject.html
+	fi
 fi
 
 chown -R www-data:www-data /var/www/html/
